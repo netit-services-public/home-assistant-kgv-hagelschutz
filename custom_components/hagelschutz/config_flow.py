d7c0053958e51d6e90fe483b3571a7a7cfe69366
netit-services-public <wh@netit-services.com>
2025-06-26 16:18:52 +0200

    Update
    Integration überarbeitet und UI-fähig gemacht

---
+++ custom_components/hagelschutz/config_flow.py
@@ -0,0 +1,19 @@
+from homeassistant import config_entries
+import voluptuous as vol
+
+from .const import DOMAIN
+
+class HagelschutzConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="Hagelalarm", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("device_id"): str,
+                vol.Optional("hwtype_id", default="188"): str,
+            })
+        )
--- custom_components/hagelschutz/sensor.py
+++ custom_components/hagelschutz/sensor.py
@@ -1,51 +1,28 @@
-import logging
-import voluptuous as vol
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_NAME
-import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
-import requests
 
-_LOGGER = logging.getLogger(__name__)
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    device_id = config_entry.data["device_id"]
+    hwtype_id = config_entry.data.get("hwtype_id", "188")
+    async_add_entities([HagelSensor(device_id, hwtype_id)], True)
 
-CONF_DEVICE_ID = "device_id"
-CONF_HWTYPE_ID = "hwtype_id"
-DEFAULT_NAME = "Hagelalarm"
-DEFAULT_HWTYPE_ID = 188
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_DEVICE_ID): cv.string,
-    vol.Optional(CONF_HWTYPE_ID, default=DEFAULT_HWTYPE_ID): cv.positive_int,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string
-})
-
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    name = config[CONF_NAME]
-    device_id = config[CONF_DEVICE_ID]
-    hwtype_id = config[CONF_HWTYPE_ID]
-    add_entities([HagelAlarmSensor(name, device_id, hwtype_id)], True)
-
-class HagelAlarmSensor(Entity):
-    def __init__(self, name, device_id, hwtype_id):
-        self._name = name
+class HagelSensor(Entity):
+    def __init__(self, device_id, hwtype_id):
         self._device_id = device_id
         self._hwtype_id = hwtype_id
         self._state = None
 
     @property
     def name(self):
-        return self._name
+        return "Hagelalarm"
 
     @property
     def state(self):
         return self._state
 
-    def update(self):
+    async def async_update(self):
+        import aiohttp
         url = f"https://meteo.netitservices.com/api/v0/devices/{self._device_id}/poll?hwtypeId={self._hwtype_id}"
-        try:
-            response = requests.get(url, timeout=10)
-            response.raise_for_status()
-            self._state = response.json().get("hailState")
-        except Exception as e:
-            _LOGGER.error("Fehler beim Abrufen des Hagelalarms: %s", e)
-            self._state = "unavailable"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                data = await resp.json()
+                self._state = data.get("hailState", None)
