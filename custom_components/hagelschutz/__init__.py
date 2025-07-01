7a539b7fe690f27014ea153f5977ab541ea7afb6
netit-services-public <wh@netit-services.com>
2025-07-01 11:01:10 +0200

    Changes

--- custom_components/hagelschutz/__init__.py
+++ custom_components/hagelschutz/__init__.py
@@ -1,23 +1,19 @@
-
 """Initialisierung der Hagelschutz-Integration."""
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
 
-
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Setup über YAML (optional, aber leer lassen)."""
     return True
-
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    """Setup bei Konfiguration über das UI."""
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Entlädt die Integration und Sensoren korrekt."""
     return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
